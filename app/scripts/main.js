57c4c87b2a46ed9be61685db0eda6877e8c4582b
Tomo Masakura <tomo.masakura@gmail.com>
2014-12-15 16:59:40 +0900

    メソッドチェーンでテストできるPageObjectのラッパーを作成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,47 @@
 (function () {
   'use strict';
 
-  var AppPage = Testmator.PageObject.extend({});
+  var ModalPage = Testmator.PageObject.extend({
+    clickCloseDemoModal: function () {
+      this.click('#close-demo-modal');
+      return this.switchParent();
+    },
+    isShown: function () {
+      return this.$el.hasClass('in');
+    }
+  });
 
-  console.log((new AppPage()) instanceof Testmator.PageObject);
+  var AppPage = Testmator.PageObject.extend({
+    switchModal: function () {
+      return new ModalPage({
+        el: this.$('#myModal'),
+        parent: this
+      });
+    },
+    clickLaunchDemoModal: function () {
+      this.click('#launch-demo-modal');
+      return this.switchModal();
+    }
+  });
+
+  var app = new AppPage();
+
+  $(document).on('click', '#test1', function () {
+    Testmator.wrap(app)
+      .test(function (app) {
+        console.log(app.switchModal().isShown());
+      })
+      .action('clickLaunchDemoModal')
+      .scope(function (dialog) {
+        dialog.clickCloseDemoModal();
+      })
+      .test(function (app) {
+        console.log(!app.switchModal().isShown());
+      })
+      .done(function () { console.log('DONE'); });
+  });
+
+  $(document).on('show.bs.modal shown.bs.modal hide.bs.modal hidden.bs.modal', function (e) {
+    console.log(e.type);
+  });
 })();
--- app/scripts/testmator.js
+++ app/scripts/testmator.js
@@ -6,12 +6,14 @@ var Testmator = (function ($, _) { // jshint ignore:line
     var parent = this;
     var child;
 
-    child = function () { return parent.apply(parent, arguments); };
+    if (prototypePropeties && _.has(prototypePropeties, 'constructor')) {
+      child = prototypePropeties.constructor;
+    } else {
+      child = function () { return parent.apply(this, arguments); };
+    }
 
     // Extend static properties.
-    if (staticProperties) {
-      _.extend(child, parent, staticProperties);
-    }
+    _.extend(child, parent, staticProperties);
 
     // Extend constructor.
     var Surrogate = function () { this.constructor = child; };
@@ -24,6 +26,90 @@ var Testmator = (function ($, _) { // jshint ignore:line
     }
 
     return child;
+  };
+
+  // Wrap automator from page object.
+  var wrapAutomator = function (page) {
+    var promise = Testmator.$.Deferred().resolve(page).promise();
+
+    var wrap = function (promise) {
+      var then = function () {
+        return wrap(promise.then.apply(promise, arguments));
+      };
+
+      var done = function () {
+        return wrap(promise.done.apply(promise, arguments));
+      };
+
+      return {
+        getPage: function () {
+          return page;
+        },
+        action: function (filter) {
+          return then(function (target) {
+            return filter.call(target, target);
+          });
+        },
+        scope: function (filter) {
+          return then(function (target) {
+            return filter.call(target, target) || page;
+          });
+        },
+        test: function (filter) {
+          return then(function (target) {
+            filter.call(target, target);
+            return page;
+          });
+        },
+        done: function () {
+          return done.apply(promise, arguments);
+        }
+      };
+    };
+
+    return wrap(promise);
+  };
+
+  // Use named action.
+  // ex: .action('clickAt', 0)
+  var appendNamedAction = function (automator) {
+    var action = function () {
+      var args = _.toArray(arguments);
+      var methodName = args.shift();
+
+      if (_.isString(methodName)) {
+        // .action('clickAt', 0)
+        return automator.action.call(automator, function (target) {
+          return target[methodName].apply(target, args);
+        });
+      } else {
+        // .action(function (target) { target.clickAt(0); })
+        return automator.action.apply(automator, arguments);
+      }
+    };
+
+    var wrapped = _.chain(automator)
+      .functions()
+      .without('getPage')
+      .map(function (name) {
+        return [
+          name,
+          function () { return appendNamedAction(automator[name].apply(automator, arguments)); }
+        ];
+      })
+      .object()
+      .value();
+
+    _.extend(wrapped, {
+      getpage: function () {
+        return automator.getPage();
+      },
+      action: function () {
+        return appendNamedAction(action.apply(automator, arguments));
+      }
+    });
+
+    return wrapped;
   };
 
   return {
@@ -39,7 +125,7 @@ var Testmator = (function ($, _) { // jshint ignore:line
         this.parent = options && options.parent;
 
         if (_.isFunction(this.initialize)) {
-          this.initialize(options);
+          this.initialize.apply(this, arguments);
         }
       };
 
@@ -48,7 +134,7 @@ var Testmator = (function ($, _) { // jshint ignore:line
           return this.$el.find.apply(this.$el, arguments);
         },
         click: function (selector) {
-          this.$(selector).click(PageObject.clickEventName);
+          this.$(selector).trigger(PageObject.clickEventName);
           return this;
         },
         switchParent: function () {
@@ -63,6 +149,9 @@ var Testmator = (function ($, _) { // jshint ignore:line
       PageObject.extend = extend;
 
       return PageObject;
-    })()
+    })(),
+    wrap: function (page) {
+      return appendNamedAction(wrapAutomator(page));
+    }
   };
 })(jQuery || $, _);
